bb69bce0d3c95e788a677bbe204626dec12d1769
Henrique Dezani <professorhenriquedezani@gmail.com>
2025-04-03 08:53:25 -0300

    obtem cotação via API

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,29 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-void main() {
-  runApp(ConversorApp());
-}
+void main() => runApp(ConversorApp());
 
 class ConversorApp extends StatelessWidget {
 
-  void converter() {}
+  var txtReais = TextEditingController();
+  var txtDolares = TextEditingController();
+
+  void converter() async {
+
+    // obtém a cotação do dolar via API
+    var url = 'https://economia.awesomeapi.com.br/json/last/USD-BRL';
+    var response = await http.get(Uri.parse(url));
+    var dados = json.decode(response.body);
+    double cotacao = double.parse(dados["USDBRL"]["bid"]);
+
+    // calcula o valor a partir da cotação
+    double reais = double.parse(txtReais.text);
+    double resultado = reais / cotacao;
+    txtDolares.text = resultado.toString();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +35,7 @@ class ConversorApp extends StatelessWidget {
           child: Column(
             children: [
               TextField(
+                controller: txtReais,
                 decoration: InputDecoration(
                   labelText: "Reais",
                   border: OutlineInputBorder(),
@@ -27,6 +43,7 @@ class ConversorApp extends StatelessWidget {
               ),
               Container(height: 10,),
               TextField(
+                controller: txtDolares,
                 decoration: InputDecoration(
                   labelText: "Dólar",
                   border: OutlineInputBorder(),
